f2678e62e0ab47896fba7bbba67b6f4779cc1703
Andrey Bakuta <anb@anahoret.com>
2015-01-13 10:08:09 +0200

    Finished workshop.

--- task15_async.js
+++ task15_async.js
@@ -1,14 +1,32 @@
+//function loadUsers(userIds, load, done) {
+  //var users = [],
+    //loadedUsers = {};
+
+  //userIds.forEach(function(i) {
+    //load(i, function(user) {
+      //loadedUsers[i] = user;
+
+      //if (Object.keys(loadedUsers).length === userIds.length) {
+        //done(userIds.map(function(id) {
+          //return loadedUsers[id];
+        //}));
+      //}
+    //});
+  //});
+//}
+
+//module.exports = loadUsers
+
 function loadUsers(userIds, load, done) {
-  var users = [];
+  var completed = 0
+  var users = []
 
-  for (var i = 0; i < userIds.length; i++) {
-    load(userIds[i], function(user) {
-      console.log(user);
-    })
-
-    users.push(load(userIds[i]));
-  }
-  done(users);
+  userIds.forEach(function(id, index) {
+    load(id, function(user) {
+      users[index] = user
+      if (++completed === userIds.length) return done(users)
+    });
+  });
 }
 
 module.exports = loadUsers
